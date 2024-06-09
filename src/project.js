37f6878c0f4c8dc27c7d59c514d7d36533ebda9f
LukaTsanavaBTU <luka.tsanava.1@btu.edu.ge>
2024-06-09 17:14:04 +0400

    Add setter functions for project factory function

--- src/project.js
+++ src/project.js
@@ -1,4 +1,4 @@
-export default function(title, icon, priority) {
+export default function(title, description, icon, priority) {
     let list = [];
     function addTodo(todoItem) {
         list.push(todoItem);
@@ -6,5 +6,17 @@ export default function(title, icon, priority) {
     function removeTodo(){
         //figure this out later
     }
-    return {list, addTodo, removeTodo};
+    function setTitle(newTitle) {
+        title = newTitle;
+    }
+    function setDesc(newDesc) {
+        description = newDesc;
+    }
+    function setIcon(newIcon) {
+        icon = newIcon;
+    }
+    function setPriority(newPrio) {
+        priority = newPrio;
+    }
+    return {list, addTodo, removeTodo, setTitle, setDesc, setIcon, setPriority};
 }
